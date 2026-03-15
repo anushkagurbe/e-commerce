5a1d5320613168ca4eb4e4a89bb4fa50dde5247f
anushkagurbe <argurbe@gmail.com>
2026-03-16 00:48:58 +0530

    add updateOrderStatus route

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
 import { isAdminMiddleware, isUserMiddleware } from "../middlewares/role.middlewares.js";
-import { cancelOrderController, createOrderController, getOrdersController, getSingleOrderDetails } from "../controllers/order.controllers.js";
+import { cancelOrderController, changeOrderStatusController, createOrderController, getOrdersController, getSingleOrderDetails } from "../controllers/order.controllers.js";
 
 let router = express.Router();
 
@@ -9,5 +9,6 @@ router.post("/", authMiddleware, isUserMiddleware, createOrderController);
 router.get("/", authMiddleware, isUserMiddleware, getOrdersController);
 router.get("/:orderId", authMiddleware, isUserMiddleware, getSingleOrderDetails);
 router.delete("/:orderId", authMiddleware, isUserMiddleware, cancelOrderController);
+router.patch("/:orderId", authMiddleware, isAdminMiddleware, changeOrderStatusController);
 
 export default router;
